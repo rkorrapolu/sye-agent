83184d7fdb5572174850e57577aff138555ea7ef
J Wang <jijwang@gmail.com>
2025-10-27 03:33:45 +0000

    redis access param

--- backend/redisconn.py
+++ backend/redisconn.py
@@ -5,15 +5,34 @@
 
 import redis
 
-r = redis.Redis(
-    host='redis-11623.c253.us-central1-1.gce.redns.redis-cloud.com',
-    port=11623,
-    decode_responses=True,
-    username="default",
-    password="3e61Hp9yvrxAu2Tht1XPXeIDSs9oL1xK",
-)
+class rclient:
+    def __init__(self):
+        self.r = redis.Redis(
+            host=os.getenv("REDIS_HOST", "localhost"),
+            port=os.getenv("REDIS_PORT", "6379"),
+            decode_responses=True,
+            username=os.getenv("REDIS_USER", "default"),
+            password=os.getenv("REDIS_PASSWORD", "")
+    )
+    
+    def create(self, key: str) -> str:
+        if self.r.exists(key):
+            return key + " already exist"
+        else:
+            self.r.set(key, '')
+            return key + " created"
+    
+    def save(self, key:str, val:str) -> None:
+        self.r.set(key, val)
 
-success = r.set('foo', 'bar')
+    def get(self, key:str) -> str:
+        return self.r.get(key)
+
+r = rclient()
+
+print(r.create('foo'))
+print(r.create('foo'))
+success = r.save('foo', 'bar')
 # True
 
 result = r.get('foo')
